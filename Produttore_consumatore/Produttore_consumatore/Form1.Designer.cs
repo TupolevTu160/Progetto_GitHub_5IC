1d36ea54537f96002b82394a72720294577918c6
TupolevTu160 <74194286+TupolevTu160@users.noreply.github.com>
2021-01-10 17:58:29 +0100

    Aggiornamento lavoro del gruppo

--- Produttore_consumatore/Produttore_consumatore/Form1.Designer.cs
+++ Produttore_consumatore/Produttore_consumatore/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btn_crea = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.btn_crea_casual = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -47,7 +48,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.txt_costo1 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dtv_tabella)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -55,6 +55,13 @@
             // 
             // dtv_tabella
             // 
+            this.dtv_tabella.AllowUserToAddRows = false;
+            this.dtv_tabella.AllowUserToDeleteRows = false;
+            this.dtv_tabella.AllowUserToResizeColumns = false;
+            this.dtv_tabella.AllowUserToResizeRows = false;
+            this.dtv_tabella.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dtv_tabella.BackgroundColor = System.Drawing.Color.Gold;
             this.dtv_tabella.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dtv_tabella.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -65,6 +72,7 @@
             this.dtv_tabella.RowTemplate.Height = 24;
             this.dtv_tabella.Size = new System.Drawing.Size(1210, 368);
             this.dtv_tabella.TabIndex = 0;
+            this.dtv_tabella.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dtv_tabella_KeyPress);
             // 
             // groupBox1
             // 
@@ -100,6 +108,7 @@
             this.txt_consumatore.Name = "txt_consumatore";
             this.txt_consumatore.Size = new System.Drawing.Size(48, 28);
             this.txt_consumatore.TabIndex = 3;
+            this.txt_consumatore.TextChanged += new System.EventHandler(this.txt_consumatore_TextChanged);
             this.txt_consumatore.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_consumatore_KeyPress);
             // 
             // label2
@@ -119,6 +128,7 @@
             this.txt_produttore.Name = "txt_produttore";
             this.txt_produttore.Size = new System.Drawing.Size(48, 28);
             this.txt_produttore.TabIndex = 2;
+            this.txt_produttore.TextChanged += new System.EventHandler(this.txt_produttore_TextChanged);
             this.txt_produttore.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_produttore_KeyPress_1);
             // 
             // label1
@@ -163,6 +173,17 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Genera casuali";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(482, 128);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(135, 24);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Errore, riprova!";
+            this.label8.Visible = false;
+            // 
             // btn_crea_casual
             // 
             this.btn_crea_casual.BackColor = System.Drawing.Color.LightGreen;
@@ -176,6 +197,9 @@
             // 
             // label6
             // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(362, 118);
@@ -186,6 +210,9 @@
             // 
             // label5
             // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(362, 53);
@@ -246,17 +273,6 @@
             this.label4.TabIndex = 1;
             this.label4.Text = "Range di costi";
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.ForeColor = System.Drawing.Color.Red;
-            this.label8.Location = new System.Drawing.Point(482, 128);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(135, 24);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "Errore, riprova!";
-            this.label8.Visible = false;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -269,8 +285,8 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Produttore-Consumatore";
             ((System.ComponentModel.ISupportInitialize)(this.dtv_tabella)).EndInit();
             this.groupBox1.ResumeLayout(false);
